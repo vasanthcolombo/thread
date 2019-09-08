8cd643a962072426a9e91667b9cafe825eedb0e9
vasanth <vasanthcolombo@gmail.com>
2019-09-08 13:53:44 +0800

    initial commit

---
+++ src/Thread.cpp
@@ -0,0 +1,28 @@
+//
+// Created by vasan on 9/8/2019.
+//
+
+#include "../include/Thread.h"
+
+Thread::Thread(int id) : _id(id){
+    stopped = true;
+}
+void Thread::start() {
+    stopped = false;
+    t = make_unique<thread>(&Thread::run, this);
+}
+
+void Thread::stop() {
+    stopped = true;
+}
+void Thread::join() {
+    t->join();
+}
+
+int Thread::get_id() {
+    return _id;
+}
+
+bool Thread::is_stopped() {
+    return stopped;
+}
